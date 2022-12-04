1bf15b161b9a51e0c13217522e2f416b94b758db
gustavo <gustavo.isaac@gmail.com>
2022-12-04 13:25:10 +0000

    HomeViewModel

---
+++ app/src/main/java/com/artemissoftware/firegallery/screens/home/HomeViewModel.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.firegallery.screens.home
+
+import com.artemissoftware.domain.usecases.GetUserUseCase
+import com.artemissoftware.firegallery.MainEvents
+import com.artemissoftware.firegallery.screens.picturedetail.PictureDetailEvents
+import com.artemissoftware.firegallery.ui.FGBaseEventViewModel
+import com.artemissoftware.firegallery.ui.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(): FGBaseEventViewModel<MainEvents>() {
+
+    override fun onTriggerEvent(event: MainEvents) {
+        when(event){
+            is MainEvents.ExecuteDeepLink -> {
+                sendUiEvent(UiEvent.DeepLink)
+            }
+        }
+    }
+}
